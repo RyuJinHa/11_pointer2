27b7419868dd6e967d9294a9451cba4ad43d9c49
RyuJinHa <bb_jinha98@naver.com>
2018-11-15 16:03:47 +0900

    5

--- main.c
+++ main.c
@@ -2,21 +2,56 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-
-char*proverb="All that glisters is not gold";
-void setPointer(char**q)
-	{	
-		*q=proverb;
-	}	
-
+	int sum(int a, int b)
+	{
+		return a+b;
+	}
+	int sub(int a, int b)
+	{
+		return a-b;
+	}
+	int mul(int a, int b)
+	{
+		return a*b;
+	}
+	
+	int divide(int a, int b)
+	{
+		return a/b;
+	}
 
 int main(int argc, char *argv[]) {
-	char*p="zzz";
-	setPointer(&p);
-	printf("%s\n", p);
 	
+	int a,b;
+	char op;
+	int(*calcfunc)(int,int);
+	int output;
 
+
+	printf("숫자와 연산자를 입력하세요\n");
+	scanf(" %d %c %d",&a,&op, &b);
+	
+	
+	
+	switch(op)
+	{
+		case'+':
+			calcfunc=sum;
+			break;
+		
+		case'-':
+			calcfunc=sub;
+			break;
+		case'*':
+			calcfunc=mul;
+			break;
+		case'/':
+			calcfunc=divide;
+			break;
+		
+	}
+
+printf("result: %i",calcfunc(a,b));
 	
 	
 	return 0;
